19197c614a89ae7bf37ac22dd667839f866ce7c4
Kris Heinrich <krisheinrich@gmail.com>
2020-06-29 22:28:51 -0700

    Add loading spinner and styles for new quotes; fix selectors

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-const quoteContainer = document.getElementById('quote-container');
+const quoteContainer = document.querySelector('.quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
-const twitterBtn = document.getElementById('twitter');
+const twitterBtn = document.querySelector('.twitter-button');
 const newQuoteBtn = document.querySelector('.new-quote');
+const loader = document.querySelector('.loader');
+
+// event listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function loaded() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
 
 async function getQuote() {
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const url = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
+    loading();
     const res = await fetch(proxyUrl + url);
     const data = await res.json();
+    
     authorText.innerHTML = data.quoteAuthor || 'Unknown';
 
     if (data.quoteText.length > 50) {
@@ -18,12 +37,19 @@ async function getQuote() {
       quoteText.classList.remove('long-quote');
     }
     quoteText.innerHTML = data.quoteText;
+    loaded();
   } catch (error) {
     getQuote();
     console.error("No quote :(", error);
   }
 }
 
-newQuoteBtn.addEventListener('click', getQuote);
+function tweetQuote() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
+}
 
+// on load
 getQuote();
