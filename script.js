fdd488815bd4209b592514dcda0e707a6c32a073
Kris Heinrich <krisheinrich@gmail.com>
2020-06-29 22:42:41 -0700

    Limit attempts to get new quote if request fails

--- script.js
+++ script.js
@@ -9,38 +9,49 @@ const loader = document.querySelector('.loader');
 newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click', tweetQuote);
 
-function loading() {
+function showLoader() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-function loaded() {
+function hideLoader() {
   if (!loader.hidden) {
     quoteContainer.hidden = false;
     loader.hidden = true;
   }
 }
 
+let apiAttempts = 0;
+
 async function getQuote() {
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const url = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
-    loading();
+    showLoader();
+    ++apiAttempts;
     const res = await fetch(proxyUrl + url);
     const data = await res.json();
-    
-    authorText.innerHTML = data.quoteAuthor || 'Unknown';
+
+    authorText.innerText = data.quoteAuthor || 'Unknown';
 
     if (data.quoteText.length > 50) {
       quoteText.classList.add('long-quote');
     } else {
       quoteText.classList.remove('long-quote');
     }
-    quoteText.innerHTML = data.quoteText;
-    loaded();
+    quoteText.innerText = data.quoteText;
+    hideLoader();
+    apiAttempts = 0;
   } catch (error) {
-    getQuote();
+    // try again
     console.error("No quote :(", error);
+    if (apiAttempts < 5) {
+      getQuote();
+    } else {
+      authorText.innerText = 'No quote found';
+      quoteText.innerText = 'Uh no!';
+      hideLoader();
+    }
   }
 }
 
